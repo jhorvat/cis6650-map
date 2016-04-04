7bb65ad1a3da4757ab5cc2549a4ca896c4f51abc
xenakisin58 <david_wickland@hotmail.com>
2016-04-04 00:59:08 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,21 +1,99 @@
 // NOT DONE, Dave and I (Curtis) are going to keep adding values to these, but this will be the format (mostly)
 
-
+//-------------- WARD 1 -------------------
 var ward1 = {
     var general = {
-        total_population: 100,
-        youth_population: 15,
-        immigrant_population: 40,
-        area: 50,
+        total_population: 100, //summarize by ward from neighbourhood
+        youth_population: 15, //samesies
+        immigrant_population: 40, //samesies again
+        area: 15.3,
+    };
+
+    var food = {
+        market_basket: 50, //still need to compute by ward
+        community_kitchens: 1,
+        food_pantries: 11,
+        community_gardens: 4,
+        bus_stops: 123,
+        daily_stops: 6210,
+        food_vendors: 10,
+    };
+
+    var income = {
+        living_wage: 45000,
+        uneducated: 20.2,
+        lico_pre_tax: 10,
+        lico_post_tax: 20,
+        income_under_30k: 50,
+        median_fam: 55000,
+    };
+
+    var employment = {
+        government_transfer_payments_pct: 20,
+        unemployment_rate: 15,
+        commuter_pct: 20,
+    };
+
+    var housing = {
+        average_rent: 500,
+    };
+};
+
+//-------------- WARD 2 -------------------
+var ward2 = {
+    var general = {
+        total_population: 200,
+        youth_population: 20,
+        immigrant_population: 23,
+        area: 10.1,
     };
 
     var food = {
         market_basket: 50,
         community_kitchens: 3,
-        food_pantries: 2,
+        food_pantries: 4,
+        community_gardens: 2,
+        bus_stops: 33,
+        daily_stops: 2248,
+        food_vendors: 3,
+    };
+
+    var income = {
+        living_wage: 45000,
+        uneducated: 20.2,
+        lico_pre_tax: 10,
+        lico_post_tax: 20,
+        income_under_30k: 50,
+        median_fam: 55000,
+    };
+
+    var employment = {
+        government_transfer_payments_pct: 20,
+        unemployment_rate: 15,
+        commuter_pct: 20,
+    };
+
+    var housing = {
+        average_rent: 500,
+    };
+};
+
+//-------------- WARD 3 -------------------
+var ward3 = {
+    var general = {
+        total_population: 200,
+        youth_population: 20,
+        immigrant_population: 23,
+        area: 10.4,
+    };
+
+    var food = {
+        market_basket: 50, //...
+        community_kitchens: 0,
+        food_pantries: 6,
         community_gardens: 4,
-        bus_stops: 25,
-        daily_stops: 200,
+        bus_stops: 77,
+        daily_stops: 4903,
         food_vendors: 18,
     };
 
@@ -39,22 +117,101 @@ var ward1 = {
     };
 };
 
-var ward2 = {
+//-------------- WARD 4 -------------------
+var ward4 = {
     var general = {
         total_population: 200,
         youth_population: 20,
         immigrant_population: 23,
-        area: 44,
+        area: 12.8,
     };
 
     var food = {
-        market_basket: 50,
-        community_kitchens: 3,
-        food_pantries: 2,
+        market_basket: 50, //...
+        community_kitchens: 0,
+        food_pantries: 4,
+        community_gardens: 3,
+        bus_stops: 80,
+        daily_stops: 4585,
+        food_vendors: 9,
+    };
+
+    var income = {
+        living_wage: 45000,
+        uneducated: 20.2,
+        lico_pre_tax: 10,
+        lico_post_tax: 20,
+        income_under_30k: 50,
+        median_fam: 55000,
+    };
+
+    var employment = {
+        government_transfer_payments_pct: 20,
+        unemployment_rate: 15,
+        commuter_pct: 20,
+    };
+
+    var housing = {
+        average_rent: 500,
+    };
+};
+
+//-------------- WARD 5 -------------------
+var ward5 = {
+    var general = {
+        total_population: 200,
+        youth_population: 20,
+        immigrant_population: 23,
+        area: 13.8,
+    };
+
+    var food = {
+        market_basket: 50, //...
+        community_kitchens: 0,
+        food_pantries: 1,
         community_gardens: 4,
-        bus_stops: 25,
-        daily_stops: 200,
-        food_vendors: 18,
+        bus_stops: 106,
+        daily_stops: 6635,
+        food_vendors: 10,
+    };
+
+    var income = {
+        living_wage: 45000,
+        uneducated: 20.2,
+        lico_pre_tax: 10,
+        lico_post_tax: 20,
+        income_under_30k: 50,
+        median_fam: 55000,
+    };
+
+    var employment = {
+        government_transfer_payments_pct: 20,
+        unemployment_rate: 15,
+        commuter_pct: 20,
+    };
+
+    var housing = {
+        average_rent: 500,
+    };
+};
+
+//-------------- WARD 6 -------------------
+var ward6 = {
+    var general = {
+        total_population: 200,
+        youth_population: 20,
+        immigrant_population: 23,
+        area: 27.4,
+    };
+
+    var food = {
+        market_basket: 50, //...
+        community_kitchens: 0,
+        food_pantries: 0,
+        community_gardens: 0,
+        bus_stops: 79,
+        daily_stops: 5857,
+        food_vendors: 6,
     };
 
     var income = {
